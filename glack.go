add98da250c655f633438a7790e9eca6edeb0682
Ryan Hatfield <ryan@ryan.bio>
2015-12-16 08:15:44 -0600

    Add version flag, enable stdin

--- glack.go
+++ glack.go
@@ -4,27 +4,37 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/user"
 )
 
+var version = "1.0.0"
+var showVersion = flag.Bool("version", false, "Show the version ("+version+")")
 var token = flag.String("token", "", "Token for Slack API")
 var saveToken = flag.Bool("save-token", false, "Save the slack token in ~/.glack")
 var channel = flag.String("channel", "#general", "Slack channel to send message to")
 var username = flag.String("username", "Glack", "Name of the bot user to send as")
 var icon = flag.String("icon", ":shoe:", "Emoji icon for the message")
 
+func getHomeDir() (dir string, err error) {
+	usr, err := user.Current()
+	return usr.HomeDir, err
+}
+
+func printVersion() {
+	if *showVersion {
+		fmt.Fprintln(os.Stdout, version)
+		os.Exit(0)
+	}
+}
+
 func main() {
 	flag.Parse()
-
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
+	printVersion()
 
 	if *token == "" {
-		file, err := os.Open(usr.HomeDir + "/.glack")
+		home, err := getHomeDir()
+		file, err := os.Open(home + "/.glack")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Token was not set.\n%+v\n", err)
 			os.Exit(1)
@@ -45,7 +55,8 @@ func main() {
 	}
 
 	if *saveToken {
-		file, err := os.Create(usr.HomeDir + "/.glack")
+		home, err := getHomeDir()
+		file, err := os.Create(home + "/.glack")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Token was not saved.\n%+v\n", err)
 			os.Exit(1)
@@ -62,21 +73,26 @@ func main() {
 		fmt.Fprintln(os.Stdout, "Token was saved.")
 	}
 
-	if flag.NArg() < 1 && *saveToken {
-		return
-	} else if flag.NArg() < 1 {
-		fmt.Fprintln(os.Stderr, "No message text provided.")
-		os.Exit(1)
-	}
-
 	c := New(*token)
-	m := Message{Channel: *channel, Message: flag.Arg(0), Username: *username, Icon: *icon}
-
-	id, err := c.Send(&m)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Send Command Failed:\n%v\n", err)
-		os.Exit(1)
+	s := func(channel, message, username, icon string) {
+		m := Message{Channel: channel, Message: message, Username: username, Icon: icon}
+		id, err := c.Send(&m)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Send Command Failed:\n%v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "Glack Message sent! Id: %v\n", id)
 	}
-	fmt.Fprintf(os.Stdout, "Glack Message sent! Id: %v\n", id)
+
+	if flag.NArg() >= 1 {
+		s(*channel, flag.Arg(0), *username, *icon)
+	} else {
+		//read from stdin
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			s(*channel, scanner.Text(), *username, *icon)
+		}
+	}
+
 	os.Exit(0)
 }
